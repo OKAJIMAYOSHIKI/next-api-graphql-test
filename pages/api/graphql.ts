7863072a4447de9c77704965619d4aaf89edcc07
YOSHIKIOKAJIMA <yoshiki.okajima@relic.co.jp>
2022-12-11 20:03:24 +0900

    GraphQLサーバー作成

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,41 +1,67 @@
-import { ApolloServer, gql } from 'apollo-server-micro';
+import { ApolloServer } from '@apollo/server';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { gql } from 'graphql-tag';
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
 
 const typeDefs = gql`
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    hello: String
+    books: [Book]
   }
 `;
 
+// A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => 'Hello World',
+    books: () => books,
   },
 };
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-const startServer = apolloServer.start();
-
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://studio.apollographql.com'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  if (req.method === 'OPTIONS') {
-    res.end();
-    return false;
-  }
-
-  await startServer;
-  await apolloServer.createHandler({
-    path: '/api/graphql',
-  })(req, res);
-}
+export default startServerAndCreateNextHandler(apolloServer, {
+  context: async (req: NextApiRequest, res: NextApiResponse) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader(
+      'Access-Control-Allow-Origin',
+      'https://studio.apollographql.com'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    if (req.method === 'OPTIONS') {
+      res.end();
+      return false;
+    }
+    return {
+      req,
+      res,
+    };
+  },
+});
 
 export const config = {
   api: {
